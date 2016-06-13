1995b7ac7d155ea55829082961ed454818783a65
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-13 13:18:09 +0200

    updated names

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ docker create --restart=always \
   -v /var/certs:/var/certs \
   --name proxy \
   proxy_image
-docker network connect webserver_default proxy
+docker network connect webserver proxy
 docker network connect mailserver proxy
 docker network connect regapp_network proxy
 docker start proxy
