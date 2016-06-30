7ce5dfdc2ba66610985389e82a33700ca0e19dd8
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-30 23:37:03 +0200

    trying

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-docker build -t proxy_image /var/proxy
-docker kill proxy
-docker rm proxy
+docker build -t proxy_image . || \
+    (echo "ERR: proxy build failed" && \
+    exit 1)
+docker inspect proxy &> /dev/null && \
+    docker kill proxy && docker rm proxy || \
+    echo "proxy seems to be already stopped"
 docker create --restart=always \
   -i -t \
   -p 80:80 \
   -p 443:443 \
   -v /var/certs:/var/certs \
-  -v /var/regapp:/var/regapp \
   --name proxy \
   proxy_image
 docker network connect webserver proxy
 docker network connect mailserver proxy
-docker network connect regapp_network proxy
+# docker network connect regapp_network proxy
 docker start proxy
