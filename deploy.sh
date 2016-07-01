c59cb25fe4f8d7c7d005f4273545bacae508a707
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-07-01 16:59:08 +0200

    now listening on localhost

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,4 @@ docker create --restart=always \
   -v /var/certs:/var/certs \
   --name proxy \
   proxy_image
-docker network connect webserver proxy
-docker network connect mailserver proxy
-# docker network connect regapp_network proxy
 docker start proxy
